0ae52a088feb99a68ebeea099b2d0b7e3a3f3ddf
gitonga.kithinji <kithinjifelix@gmail.com>
2022-12-09 11:58:08 +0300

    Add project files.

---
+++ ICL.Mediator/Controllers/GrnController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ICL.Mediator.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GrnController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> ReceiveGRN()
+        {
+            // send middle ware
+            return Ok();
+        }
+    }
+}
